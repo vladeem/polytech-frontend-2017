05369f78085a554afb67565fd8de89a62c5ab317
Vladimir Demkin <lairotele@gmail.com>
2017-10-11 18:37:31 +0300

    Тест prettier на precommit

--- several-components/src/App.js
+++ several-components/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import CattleList from './Cattle.js';
+import React, { Component } from "react";
+import "./App.css";
+import CattleList from "./Cattle.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{border:'2px solid black',width:'60%',margin:'0 auto', padding:'25px'}}>
-			<div></div>
-           <CattleList/>
+      <div
+        className="App"
+        style={{
+          border: "2px solid black",
+          width: "60%",
+          margin: "0 auto",
+          padding: "25px"
+        }}
+      >
+        <div />
+        <CattleList />
       </div>
     );
-  } 
+  }
 }
 
 export default App;
--- several-components/src/Form.js
+++ several-components/src/Form.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-class NumberForm extends Component{
-	render() {
-		return( <form style={{marginTop:'50px'}}>
-				
-				<input type='text' onChange={this.handleChange}/>
-				<input type='button' onClick={this.clickHandle} value='Угадать'/>
-				</form>)
-	}
-	constructor(props) {
-		super(props);
-		this.state={
-			value:''
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.clickHandle = this.clickHandle.bind(this);
-	}
-	handleChange(event) {
-        this.setState({
-            value: event.target.value
-        });
-    }
-	clickHandle(){
-		this.props.addGuess(this.state.value);
-	}
-
+import React, { Component } from "react";
+class NumberForm extends Component {
+  render() {
+    return (
+      <form style={{ marginTop: "50px" }}>
+        <input type="text" onChange={this.handleChange} />
+        <input type="button" onClick={this.clickHandle} value="Угадать" />
+      </form>
+    );
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.clickHandle = this.clickHandle.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  clickHandle() {
+    this.props.addGuess(this.state.value);
+  }
 }
-	  
+
 export default NumberForm;
